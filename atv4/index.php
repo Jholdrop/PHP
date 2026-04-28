33606f181a5e98becd29151106b159e3ada32d12
Jholprop12 <higorbovolini@gmail.com>
2026-04-28 13:57:48 -0300

    documentação

--- atv4/index.php
+++ atv4/index.php
@@ -7,16 +7,17 @@
 </head>
 <body>
     <?php
+    //declaração das variaveis
     $nome = "Imperio da bola";
     $valor = 25.50 ;
     $funcionarios = 10;
     $dono = "Jair";
-    $funcionando = True;
-    echo "<h1>$nome</h1>";
-    echo "<h2>valor por pessoa: $valor</h2>";
-    echo "<p>quantidade de funcionarios: $funcionarios</p>";
-    echo "<p>CEO: $dono</p>";
-    echo "<p>Aberto: $funcionando</p>";
+    $funcionando = True; 
+    echo "<h1>$nome</h1>"; //variavel tipo STRING
+    echo "<h2>valor por pessoa: $valor</h2>"; //variavel tipo FLOAT
+    echo "<p>quantidade de funcionarios: $funcionarios</p>"; //variavel tipo INT
+    echo "<p>CEO: $dono</p>"; //variavel tipo STRING
+    echo "<p>Aberto: $funcionando</p>"; //variavel tipo BOOl
      ?>
 </body>
 </html>
